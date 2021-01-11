4bafc382fe11eb0342373a20feaad19fedb9118a
zero88 <sontt246@gmail.com>
2021-01-11 15:30:34 +0700

    Release v1.1.7

--- rubix-plat/modify-distribution.js
+++ rubix-plat/modify-distribution.js
@@ -3,20 +3,6 @@ var fs = require('fs');
 const location = './static/js/'
 var files = fs.readdirSync(location);
 
-var myArgs = process.argv.slice(2);
-
-var port = 1414;
-
-for (var i = 0; i < myArgs.length - 1; i++) {
-  if (myArgs[i] === '--port') {
-    port = myArgs[i + 1]
-  }
-}
-
-console.log("|=======================|")
-console.log(`|      PORT: ${port}       |`)
-console.log("|=======================|")
-
 files.forEach(file => {
   if (file.startsWith('app') && file.endsWith('js')) {
     var absoluteFile = `${location}/${file}`
@@ -24,8 +10,8 @@ files.forEach(file => {
       if (err) {
         return console.log(err);
       }
-      var result = data.replace(/:\d+\/api/, `:${port}/api`);
-      console.log(`Editing your file '${file}' with port '${port}'`)
+      var result = data.replace(/":\d+\/api"/, "`:${window.location.port}/api`");
+      console.log(`Editing your file '${file}' with port '\${window.location.port}'`)
       fs.writeFile(absoluteFile, result, 'utf8', function (err) {
         if (err) return console.log(err);
       });
