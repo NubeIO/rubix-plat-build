51cfd37af6c6afbbc85e71a4de77b3e4cc24c84d
ci.zero88 <sontt246@gmail.com>
2021-01-14 11:08:51 +0000

    <ci-auto-commit> Release v1.2.2

--- rubix-plat/modify-distribution.js
+++ rubix-plat/modify-distribution.js
@@ -4,18 +4,16 @@ const location = './static/js/'
 var files = fs.readdirSync(location);
 
 files.forEach(file => {
-  if (file.startsWith('app') && file.endsWith('js')) {
-    var absoluteFile = `${location}/${file}`
-    fs.readFile(absoluteFile, 'utf8', function (err, data) {
-      if (err) {
-        return console.log(err);
-      }
-      var result = data.replace(/":\d+\/api"/, "`:${window.location.port}/api`");
-      console.log(`Editing your file '${file}' with port '\${window.location.port}'`)
-      fs.writeFile(absoluteFile, result, 'utf8', function (err) {
-        if (err) return console.log(err);
-      });
+  var absoluteFile = `${location}/${file}`
+  fs.readFile(absoluteFile, 'utf8', function (err, data) {
+    if (err) {
+      return console.log(err);
+    }
+    var result = data.replace(/":\d+\/api"/g, "`:${window.location.port}/api`");
+    console.log(`Editing your file '${file}' with port '\${window.location.port}'`)
+    fs.writeFile(absoluteFile, result, 'utf8', function (err) {
+      if (err) return console.log(err);
     });
-  }
+  });
 })
 
