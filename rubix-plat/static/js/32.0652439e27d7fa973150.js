8a1a5363500bfa2aa8f4b44e90ff11e991fba4af
ci.zero88 <sontt246@gmail.com>
2021-04-19 00:42:42 +0000

    <ci-auto-commit> Release v1.6.3

---
+++ rubix-plat/static/js/32.0652439e27d7fa973150.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[32],{"+LRK":function(e,n,t){"use strict";t.r(n);var r={},s=t("psIG"),u=Object(s.a)(r,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("router-view")],1)}),[],!1,null,null,null);n.default=u.exports}}]);
+//# sourceMappingURL=32.0652439e27d7fa973150.js.map
