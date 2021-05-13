1f8680cf35a437e88b9f15813e502b52ea3f73b8
ci.zero88 <sontt246@gmail.com>
2021-05-13 08:45:21 +0000

    <ci-auto-commit> Release v1.7.0

--- rubix-plat/static/js/27.a277e2f83517a091c5ae.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{Cvoc:function(t,n,o){"use strict";o.r(n);var e={components:{Jcrop:o("CtFe").Jcrop},data:function(){return{options:{},sel:{}}}},s=o("psIG"),r=Object(s.a)(e,(function(){var t=this,n=t.$createElement,o=t._self._c||n;return o("div",[o("Jcrop",{attrs:{src:"https://d3o1694hluedf9.cloudfront.net/sierra-750.jpg",rect:[10,10,100,100],options:t.options},on:{update:function(n){t.sel=n.sel}}})],1)}),[],!1,null,null,null);n.default=r.exports}}]);
-//# sourceMappingURL=27.a277e2f83517a091c5ae.js.map
