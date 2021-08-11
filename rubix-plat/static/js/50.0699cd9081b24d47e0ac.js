eb2359dbf319d4c26aae693447c375c35f1755f7
RaiBnod <binodrayee@gmail.com>
2021-08-11 06:56:12 +0000

    <ci-auto-commit> Release v2.0.0

---
+++ rubix-plat/static/js/50.0699cd9081b24d47e0ac.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[50],{"+LRK":function(e,n,t){"use strict";t.r(n);var r={},s=t("psIG"),u=Object(s.a)(r,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("router-view")],1)}),[],!1,null,null,null);n.default=u.exports}}]);
+//# sourceMappingURL=50.0699cd9081b24d47e0ac.js.map
