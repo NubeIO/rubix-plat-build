db28e25771074fbd7302f86109c1ec9bd73e0910
RaiBnod <binodrayee@gmail.com>
2021-09-22 03:29:52 +0000

    <ci-auto-commit> Release v2.0.6

--- rubix-plat/static/js/45.261aa7638fcc08fe0bc8.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[45],{C5KM:function(t,e,n){"use strict";n.r(e);var o=n("uzYq"),r={mounted:function(){"bacnet-networks"!=this.$router.currentRoute.name&&this.getNetworks()},methods:{getNetworks:function(){var t=this;o.a.get("bacnet_master/networks").then((function(e){t.$store.commit("changeBmNetworksData",e.networks)}))}}},s=n("psIG"),u=Object(s.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("router-view")}),[],!1,null,null,null);e.default=u.exports}}]);
-//# sourceMappingURL=45.261aa7638fcc08fe0bc8.js.map
