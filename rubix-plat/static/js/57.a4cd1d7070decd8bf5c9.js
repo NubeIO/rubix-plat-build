97baf43c56effde3a5e1fcb6b7745503ade4b827
RaiBnod <binodrayee@gmail.com>
2021-11-09 05:51:07 +0000

    <ci-auto-commit> Release v2.0.7

--- rubix-plat/static/js/57.a4cd1d7070decd8bf5c9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[57],{"+LRK":function(e,n,t){"use strict";t.r(n);var r={},s=t("psIG"),u=Object(s.a)(r,(function(){var e=this.$createElement,n=this._self._c||e;return n("div",[n("router-view")],1)}),[],!1,null,null,null);n.default=u.exports}}]);
-//# sourceMappingURL=57.a4cd1d7070decd8bf5c9.js.map
---
+++ rubix-plat/static/js/66.955a9cdc2864e591d391.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[66],{C5KM:function(t,e,n){"use strict";n.r(e);var o=n("uzYq"),r={mounted:function(){"bacnet-networks"!=this.$router.currentRoute.name&&this.getNetworks()},methods:{getNetworks:function(){var t=this;o.a.get("bacnet_master/networks").then((function(e){t.$store.commit("changeBmNetworksData",e.networks)}))}}},s=n("psIG"),u=Object(s.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("router-view")}),[],!1,null,null,null);e.default=u.exports}}]);
+//# sourceMappingURL=66.955a9cdc2864e591d391.js.map
