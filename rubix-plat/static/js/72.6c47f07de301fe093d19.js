2f9446864e741f4b441901f37444f8e54aa13aa8
RaiBnod <binodrayee@gmail.com>
2022-04-07 08:26:53 +0000

    <ci-auto-commit> Release v2.5.0

---
+++ rubix-plat/static/js/72.6c47f07de301fe093d19.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[72],{Cvoc:function(t,n,o){"use strict";o.r(n);var e={components:{Jcrop:o("CtFe").Jcrop},data:function(){return{options:{},sel:{}}}},s=o("psIG"),r=Object(s.a)(e,(function(){var t=this,n=t.$createElement,o=t._self._c||n;return o("div",[o("Jcrop",{attrs:{src:"https://d3o1694hluedf9.cloudfront.net/sierra-750.jpg",rect:[10,10,100,100],options:t.options},on:{update:function(n){t.sel=n.sel}}})],1)}),[],!1,null,null,null);n.default=r.exports}}]);
+//# sourceMappingURL=72.6c47f07de301fe093d19.js.map
